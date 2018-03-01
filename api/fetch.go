aaa0f47a5460c7f7d1b890dd1468a474e146eee2
ezra <linux.esdras@gmail.com>
2018-03-01 14:45:35 -0300

    Add fetch json with cache

---
+++ api/fetch.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"reflect"
+	"time"
+)
+
+func fetchJSON(url string, response interface{}, cacheTime float64) error {
+	if res, ok := getCache(url); ok != false {
+		item := res.(cacheItem)
+		if time.Now().Sub(item.Time).Minutes() < item.Cachetime {
+			reflect.ValueOf(response).Elem().Set(reflect.ValueOf(item.Data))
+			return nil
+		}
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer check(res.Body.Close)
+	err = json.NewDecoder(res.Body).Decode(response)
+
+	if err == nil {
+		putCache(url, response, cacheTime)
+	}
+
+	return err
+}
