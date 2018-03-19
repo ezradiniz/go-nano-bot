0dad6b0a302a4b02f6f38c96fb531927e5e01362
ezra <linux.esdras@gmail.com>
2018-03-19 00:26:46 -0300

    Add balance

---
+++ api/raiblocks.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+// Balance response
+type Balance struct {
+	Amount string `json:"amount"`
+}
+
+// Raiblocks raiblocks (nano) URL
+const Raiblocks = "https://raiblocks.net/account/index.php"
+
+// FetchBalance fetch user balance from raiblocks.net
+func FetchBalance(address string, response interface{}) error {
+	c := colly.NewCollector()
+
+	c.OnHTML(".npage > .row:nth-of-type(3) strong", func(e *colly.HTMLElement) {
+		text := strings.TrimSpace(e.Text)
+		reflect.ValueOf(response).Elem().FieldByName("Amount").SetString(text)
+	})
+
+	err := c.Visit(Raiblocks + "?acc=" + address)
+
+	c.Wait()
+
+	return err
+}
---
+++ handlers/balance_handler.go
@@ -0,0 +1,43 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/ezradiniz/go-nano-bot/api"
+	"github.com/ezradiniz/go-nano-bot/util"
+)
+
+// BalanceHandler ...
+func BalanceHandler(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	if len(args) == 0 {
+		check(s.ChannelMessageSend(m.ChannelID, "Please inform your address"))
+		return
+	}
+
+	balance := api.Balance{}
+
+	err := api.FetchBalance(args[0], &balance)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		check(s.ChannelMessageSend(m.ChannelID, "Sorry, an error ocurred while processing your request"))
+		return
+	}
+
+	if balance.Amount == "" {
+		check(s.ChannelMessageSend(m.ChannelID, "Invalid address"))
+		return
+	}
+
+	embed := util.MessageEmbedDefault()
+
+	embed.Author = &discordgo.MessageEmbedAuthor{
+		Name: "Address",
+		URL:  api.Raiblocks + "?acc=" + args[0],
+	}
+
+	util.AddFieldEmbed(embed, "Amount", balance.Amount)
+
+	check(s.ChannelMessageSendEmbed(m.ChannelID, embed))
+}
